a0d31a3d20ff797b25b9892db71aa6ff2f52ff4e
RohanAwhad <rohanavad007@gmail.com>
2020-04-10 15:44:48 +0530

    Added cmd for ejs file type

--- .my_cmds.sh
+++ .my_cmds.sh
@@ -14,4 +14,22 @@ function html(){
 	echo "		<!-- data goes here -->" >> "$var1"
 	echo "	</body>" >> "$var1"
 	echo "</html>" >> "$var1"
+	vim "$var1"
+}
+
+function ejs(){
+	var1="${1}.ejs"
+	touch "$var1"	
+	echo "<!DOCTYPE html>" >> "$var1"
+	echo "<html>" >> "$var1"
+	echo "	<head>" >> "$var1"
+	echo "		<!-- metadata goes here -->" >> "$var1"
+	echo "		<title>" >> "$var1"
+	echo "		</title>" >> "$var1"
+	echo "	</head>" >> "$var1"
+	echo "	<body>" >> "$var1"
+	echo "		<!-- data goes here -->" >> "$var1"
+	echo "	</body>" >> "$var1"
+	echo "</html>" >> "$var1"
+	vim "$var1"
 }
